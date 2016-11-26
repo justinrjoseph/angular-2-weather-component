a9a170a9a2225c3a8ed808527ca4b329c945f6dd
Justin Joseph <justin.joseph0@yahoo.com>
2016-11-26 13:48:21 -0500

    Set up weather icon backgrounds and colors

--- app/weather-widget/constants/constants.js
+++ app/weather-widget/constants/constants.js
@@ -3,4 +3,50 @@ exports.FORECAST_KEY = '7f5c1aa8ac29c8f5c3e9671f7e683122';
 exports.FORECAST_BASE_URL = 'https://api.darksky.net/forecast/';
 exports.GEOCODING_KEY = 'AIzaSyDVXq3hC0eSfRTYsn3Wln0YjkA9JvjitmY';
 exports.GEOCODING_BASE_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
+exports.WEATHER_COLORS = {
+    'default': {
+        'background-color': '#00bcd4',
+        'color': '#fff'
+    },
+    'clear-day': {
+        'background-color': '#22a7f0',
+        'color': '#fff'
+    },
+    'clear-night': {
+        'background-color': '#2c3e60',
+        'color': '#fff'
+    },
+    'rain': {
+        'background-color': '#5c97bf',
+        'color': '#000'
+    },
+    'snow': {
+        'background-color': '#fff',
+        'color': '#000'
+    },
+    'sleet': {
+        'background-color': '#e4fife',
+        'color': '#000'
+    },
+    'wind': {
+        'background-color': '#81cfe0',
+        'color': '#000'
+    },
+    'fog': {
+        'background-color': '#67809f',
+        'color': '#fff'
+    },
+    'cloudy': {
+        'background-color': '#bdc3c7',
+        'color': '#000'
+    },
+    'partly-cloudy-day': {
+        'background-color': '#d2d7d3',
+        'color': '#000'
+    },
+    'partly-cloudy-night': {
+        'background-color': '#34495e',
+        'color': '#000'
+    }
+};
 //# sourceMappingURL=constants.js.map
